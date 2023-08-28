47b3a03fc4fc63ccb1c9578f27306ae4aa57fce6
Cptrex <38324542+Cptrex@users.noreply.github.com>
2023-08-28 22:06:38 +0600

    added frontend folder

---
+++ SearchAggregator/Frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <SearchAggregator/>
+</template>
+
+<script>
+import SearchAggregator from './components/SearchAggregator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SearchAggregator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ SearchAggregator/Frontend/src/components/SearchAggregator.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="search">
+    <h1>Аггрегатор поисковых запросов Google Bing Yandex</h1>
+      <form @submit.prevent>
+        <input class="search__input" v-model="searchInput"/>
+        <button type="submit" class="search__button" @click="searchWebResults()">Поиск</button>
+      </form>
+      <div class="search__wrapper">
+        <h2>Google</h2>
+        <h2>Yandex</h2>
+        <h2>Bing</h2>
+      </div>
+      <p class="search__preloader" v-if="isDataLoading">Загружаем данные...</p>
+      <section class="search__section section" v-else>
+      <article class="section__article" v-for="searchData in searchResult">
+        <ul class="section__list">
+          <li class="section__item item" v-for="searchEntity in searchData">
+            <div class="item__wrapper">
+              <h3 class="item__title">{{ searchEntity.title }}</h3>
+              <a class="item__link" :href="searchEntity.url" target="_blank">{{ searchEntity.url }}</a>
+              <p class="item__description">{{ searchEntity.description }}</p>
+            </div>
+          </li>
+        </ul>
+      </article>
+    </section>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'SearchAggregator',
+  data() {
+    return {
+      searchInput: "",
+      isDataLoading: false,
+      searchResult: {
+        googleResult: [],
+        bingResult: [],
+        yandexResult: []
+      }
+    }
+  },
+  methods: {
+    searchWebResults() {
+      this.isDataLoading = true;
+      axios.get(`http://localhost:5133/api/v1/aggregator/search?searchText=${this.searchInput}`).then(response => {
+          this.searchResult = response.data;
+          this.isDataLoading = false;
+      }).catch(error => {
+        console.log(error);
+        this.isDataLoading = false;
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .search__section {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+  }
+  
+  .section__list {
+    list-style-type: none;
+  }
+
+  .section_list:not(:last-of-type) {
+    margin-bottom: 10px;
+  }
+  .item__link {
+    overflow-x: hidden;
+  }
+
+  .item__wrapper {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .section__article{
+    width:30%;
+  }
+
+  .item__description {
+    text-align: justify;
+  }
+
+  .search__wrapper {
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+  }
+
+  .search__button {
+    width: 70px;
+    height: 35px;
+    font-size: 17px;
+  }
+
+  .search__input{
+    width: 300px;
+    height: 29px;
+    font-size: 17px;
+    margin-right: 5px;
+  }
+
+  .search__preloader {
+    font-size: 25px;
+  }
+</style>
